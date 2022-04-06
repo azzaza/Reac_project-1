d6858643c6ae087b69f46e7ab3b8c581c0088c08
Захар Ясько <yasko.zakhar@gmail.com>
2022-04-06 16:17:05 +0200

    Updates

---
+++ precache-manifest.effa88433251659bf30abb64ca6b708f.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1f28e5126d071a48970323af1eee2e24",
+    "url": "/Reac_project-1/index.html"
+  },
+  {
+    "revision": "405b86dd59d4639f7ff1",
+    "url": "/Reac_project-1/static/css/main.2a6bcecf.chunk.css"
+  },
+  {
+    "revision": "54a88c7be95327fd3d8f",
+    "url": "/Reac_project-1/static/js/2.ba8cfb34.chunk.js"
+  },
+  {
+    "revision": "0142f9f3abfb73ad2f4072c1d566d6e3",
+    "url": "/Reac_project-1/static/js/2.ba8cfb34.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "405b86dd59d4639f7ff1",
+    "url": "/Reac_project-1/static/js/main.ff5eba16.chunk.js"
+  },
+  {
+    "revision": "11367f5e54a532ed6085",
+    "url": "/Reac_project-1/static/js/runtime-main.64b57dbb.js"
+  },
+  {
+    "revision": "51c934afb61ee321ae0d9f05a4b432e9",
+    "url": "/Reac_project-1/static/media/getFetch.51c934af.cjs"
+  },
+  {
+    "revision": "80b0d06ca3ae655f03df710957cb11d1",
+    "url": "/Reac_project-1/static/media/img_def.80b0d06c.jpg"
+  },
+  {
+    "revision": "9e0373c2b4410c49439dfd822c5fd16e",
+    "url": "/Reac_project-1/static/media/spiner.9e0373c2.gif"
+  }
+]);
